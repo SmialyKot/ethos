d9720e2b379e265fa7ad661fa632fb425f76f2a3
milosz <gracwgre@gmail.com>
2020-10-10 14:09:14 +0200

    Basic database implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'add_mood_button.dart';
 import 'mood_slider.dart';
+import 'database_helpers.dart';
 
 void main() => runApp(Ethos());
 
@@ -30,10 +31,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  
-  
+
   double _sliderValue = 50.0;
-  
+
+  final moodList = {
+    0.0 : 0,
+    25.0 : 1,
+    50.0 : 2,
+    75.0 : 3,
+    100.0 : 4,
+  };
+
+  _saveToDatabase(var value) async {
+    Mood mood = Mood();
+    DateTime now = new DateTime.now();
+    DateTime date = new DateTime(now.month, now.day, now.hour);
+    mood.date = date.toString();
+    mood.mood = value;
+    DatabaseHelper helper = DatabaseHelper.instance;
+    await helper.insert(mood);
+  }
+
   void _moodSlider() async {
     final selectedMood = await showDialog<double>(
       context: context,
@@ -44,6 +62,7 @@ class _HomeViewState extends State<HomeView> {
         setState(() {
           _sliderValue = selectedMood;
         });
+        _saveToDatabase(moodList[_sliderValue]);
       }
   }
   
